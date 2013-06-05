ff458182716facf8349e5cef7fd6e87a6dda5090
Cristian Jiménez <ezefire@gmail.com>
2013-06-05 11:11:38 +0200

    Nuevo commit

---
+++ EjemploGit/src/com/example/ejemplogit/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.ejemplogit;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		TextView texto= (TextView) this.findViewById(R.id.texto1);
+		texto.setText("Branch 1");
+
+		//comentario nuevo
+		// Otro comentario
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
---
+++ EjemploGit/src/com/example/ejemplogit/claseNueva.java
@@ -0,0 +1,5 @@
+package com.example.ejemplogit;
+
+public class claseNueva {
+
+}
